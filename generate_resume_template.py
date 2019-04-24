52b80400dde64e4d851e1a90f23221acc7429ba3
Timothy Vandenbrande <timothy.vandenbrande@cegeka.com>
2019-04-24 15:41:14 +0200

    update generator

--- generate_resume_template.py
+++ generate_resume_template.py
@@ -9,7 +9,7 @@ import os
 
 def get_context(resumedir, filename):
     if os.path.splitext(filename)[1] in ['.yml', '.yaml']:
-        context = yaml.load(open(resumedir + "/" + filename, 'r', encoding="utf-8"))
+        context = yaml.load(open(resumedir + "/" + filename, 'r', encoding="utf-8"), Loader=yaml.SafeLoader)
     elif os.path.splitext(filename)[1] in ['.json']:
         context = json.loads(open(resumedir + "/" + filename, 'r', encoding="utf-8").read())
     else:
@@ -18,18 +18,19 @@ def get_context(resumedir, filename):
     # print(yaml.dump(context, encoding=('utf-8')))
     return context
 
-def generate_md(outputdir, context, template):
+def generate_md(outputdir, context, md):
     env = Environment(loader = FileSystemLoader(os.path.dirname(os.path.abspath(__file__))), trim_blocks=True)
-    template = env.get_template(template)
+    template = env.get_template(md)
     with open(outputdir + "/" + os.path.splitext(filename)[0] + ".md", 'w+', encoding="utf-8") as x_file:
         rendered_template = template.render(context)
         x_file.write(rendered_template)
 
-def generate_docx(outputdir, context, template):
+def generate_docx(outputdir, context, docx):
+    template = DocxTemplate(docx)
     template.render(context)
     template.save(outputdir + "/" + os.path.splitext(filename)[0] + ".docx")
 
-docx = DocxTemplate("templates/cv_template.docx.j2")
+docx = "templates/cv_template.docx.j2"
 md = "templates/cv_template.md.j2"
 outputdir = os.getcwd() + "/output"
 resumedir = os.getcwd() + "/resume"
