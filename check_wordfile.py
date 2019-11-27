d83f009a83ac454418748de74eac66e5ed2b4a1e
Timothy Vandenbrande <timothy.vandenbrande@gmail.com>
2019-11-27 20:47:31 +0100

    fix a filechecking bug

--- check_wordfile.py
+++ check_wordfile.py
@@ -5,14 +5,26 @@ import textract
 
 text = textract.process('output/resume.docx', extension='docx')
 
+faulty = False
 for line in str(text).split('\n'):
     if "{{" in line or "}}" in line:
-        raise "Not all template variables have been resolved"
+        faulty = True
+        print(line)
     #print line
+
+if faulty:
+    print("Not all template variables have been resolved")
+    raise
 
 # text = textract.process('output/resume.md', extension='md')
 
+faulty = False
 for line in str(text).split('\n'):
     if "{{" in line or "}}" in line:
-        raise "Not all template variables have been resolved"
+        faulty = True
+        print(line)
     #print line
+
+if faulty:
+    print("Not all template variables have been resolved")
+    raise
